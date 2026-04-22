c5500c9bfff33c3f520f6248cc50a1811eb946bc
apple <apple@appleui-MacBookPro.local>
2026-04-22 12:56:36 +0900

    덧셈 뺄셈(t1~t11) 구현

--- TDDcalc/src/main/java/com/back/Calc.java
+++ TDDcalc/src/main/java/com/back/Calc.java
@@ -2,6 +2,27 @@ package com.back;
 
 public class Calc {
     public static int run(String content) {
-        return 1;
+        String[] str = content.split(" ");
+        int a = 0;
+        String b = "+";
+
+        for (String s : str) {
+            if (s.matches("[+\\-*]")) { // 연산자 체크
+                b = s;
+            } else if (s.matches("\\d+")) { // 숫자 체크
+                a = calculate(a, b, Integer.parseInt(s));
+            }
+        }
+
+        return a;
+    }
+
+    private static int calculate(int a, String b, int c) {
+
+        return switch (b) {
+            case "+" -> a + c;
+            case "-" -> a - c;
+            default -> a;
+        };
     }
 }

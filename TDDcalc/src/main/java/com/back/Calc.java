91461a56094b5c6d90fcd94628e15272cb0fbe66
apple <apple@appleui-MacBookPro.local>
2026-04-22 16:18:51 +0900

    곱셈과 음수 처리(~t14)

--- TDDcalc/src/main/java/com/back/Calc.java
+++ TDDcalc/src/main/java/com/back/Calc.java
@@ -3,25 +3,41 @@ package com.back;
 public class Calc {
     public static int run(String content) {
         String[] str = content.split(" ");
-        int a = 0;
-        String b = "+";
+        return search(str);
+    }
 
-        for (String s : str) {
-            if (s.matches("[+\\-*]")) { // 연산자 체크
-                b = s;
-            } else if (s.matches("\\d+")) { // 숫자 체크
-                a = calculate(a, b, Integer.parseInt(s));
+    private static int search(String[] str) {
+        int a = Integer.parseInt(str[0]);      // 현재 값
+        String b = "+"; // 연산자
+        int flag = 0;
+        int sum = 0;    // 누적 값
+
+        for (int i = 1; i < str.length; i++) {
+            if (str[i].matches("[+\\-*]")) {
+                if (b.equals("*")) {
+                    flag = 1;
+                } else if (b.equals("+") || b.equals("-")) {
+                    sum = calculate(sum, b, a);
+                }
+                b = str[i];
+            } else if (str[i].matches("-?\\d+")) {
+                if (flag == 1) {
+                    a = calculate(a, "*", Integer.parseInt(str[i]));
+                    flag = 0;
+                } else {
+                    a = Integer.parseInt(str[i]);
+                }
             }
         }
-
-        return a;
+        sum = calculate(sum, b, a);
+        return sum;
     }
 
     private static int calculate(int a, String b, int c) {
-
         return switch (b) {
             case "+" -> a + c;
             case "-" -> a - c;
+            case "*" -> a * c;
             default -> a;
         };
     }
